444385ff9772e9e00d896fcd9e378d9641697256
vs256 <44126680+vs256@users.noreply.github.com>
2022-02-25 19:05:50 -0800

    Revert "Revert "Merging changes into main""

---
+++ SourceCode/src/Token.java
@@ -0,0 +1,11 @@
+public interface Token {
+
+    // already pre-defined on object but just for reference
+    public boolean equals(final Object other);
+
+    public int hashCode();
+
+    public String toString();
+    //
+
+}
