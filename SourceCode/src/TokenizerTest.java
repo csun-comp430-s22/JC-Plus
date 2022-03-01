f5a1053e03d3d8cdcb85ac1043f33847a262e38c
AlexisDiaz8706 <alexis.flanigan@gmail.com>
2022-02-28 21:58:29 -0800

    beginnings of unit testing

--- SourceCode/src/TokenizerTest.java
+++ SourceCode/src/TokenizerTest.java
@@ -1,12 +1,22 @@
 import java.util.List;
 
 public class TokenizerTest {
-
-    public static void main(String[] args) throws TokenizerException {
-        // check that tokenizing empty string works
+    public static void testEmptyString() throws TokenizerException{
         Tokenizer tokenizer = new Tokenizer("");
         List<Token> tokens = tokenizer.tokenize();
         assert (tokens.size() == 0);
 
+    }
+    public static void testOnlyWhiteSpace() throws TokenizerException{
+        Tokenizer tokenizer = new Tokenizer("     ");
+        List<Token> tokens = tokenizer.tokenize();
+        assert (tokens.size() == 0);
+
+    }
+
+    public static void main(String[] args) throws TokenizerException {
+        // check that tokenizing empty string works
+       testOnlyWhiteSpace();
+       testEmptyString();
     }
 }
