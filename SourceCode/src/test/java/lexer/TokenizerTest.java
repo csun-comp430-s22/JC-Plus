53ebac3a9d56867e791975eaa6cb924eb2c8155c
vs256 <vs256@gmail.com>
2022-03-11 21:27:37 -0800

    Added test in right directory

--- SourceCode/src/test/java/lexer/TokenizerTest.java
+++ SourceCode/src/test/java/lexer/TokenizerTest.java
@@ -1,23 +1,32 @@
 package lexer;
+
 import java.util.List;
 
 public class TokenizerTest {
-    public static void testEmptyString() throws TokenizerException{
+    public static void testEmptyString() throws TokenizerException {
         Tokenizer tokenizer = new Tokenizer("");
         List<Token> tokens = tokenizer.tokenize();
         assert (tokens.size() == 0);
 
     }
-    public static void testOnlyWhiteSpace() throws TokenizerException{
+
+    public static void testOnlyWhiteSpace() throws TokenizerException {
         Tokenizer tokenizer = new Tokenizer("     ");
         List<Token> tokens = tokenizer.tokenize();
         assert (tokens.size() == 0);
 
     }
-    
+
+    public static void testThisByItself() throws TokenizerException {
+        Tokenizer tokenizer = new Tokenizer("this");
+        List<Token> tokens = tokenizer.tokenize();
+        assert (tokens.size() == 0);
+    }
+
     public static void main(String[] args) throws TokenizerException {
         // check that tokenizing empty string works
-       testOnlyWhiteSpace();
-       testEmptyString();
+        testOnlyWhiteSpace();
+        testEmptyString();
+        testThisByItself();
     }
 }
