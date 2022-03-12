a80566087e3aec063458423aa2b80069168d9295
osherboudara99 <osherboudara99@gmail.com>
2022-03-12 12:53:43 -0800

    added all tests and 100% code coverage achieved

--- SourceCode/src/test/java/lexer/TokenizerTest.java
+++ SourceCode/src/test/java/lexer/TokenizerTest.java
@@ -35,13 +35,225 @@ public class TokenizerTest {
 
     }
 
+
+    @Test
+    public void testAssignmentByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("=", new Token[] { new AssignmentToken() });
+    }
+
+    @Test
+    public void testBreakByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("break", new Token[] { new BreakToken() });
+    }
+
+    @Test
+    public void testClassByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("class", new Token[] { new ClassToken() });
+    }
+    @Test
+    public void testDivisionByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("/", new Token[] { new DivisionToken() });
+    }
+
+    @Test
+    public void testDotByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes(".", new Token[] { new DotToken() });
+    }
+
+    @Test
+    public void testElseByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("else", new Token[] { new ElseToken() });
+    }
+
+    @Test
+    public void testEqualsByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("==", new Token[] { new EqualsToken() });
+    }
+
+    @Test
+    public void testExtendsByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("extends", new Token[] { new ExtendsToken() });
+    }
+
+    @Test
+    public void testGreaterThanByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes(">", new Token[] { new GreaterThanToken() });
+    }
+
+    @Test
+    public void testIfByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("if", new Token[] { new IfToken() });
+    }
+
+    @Test
+    public void testIntByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("Int", new Token[] { new IntToken() });
+    }
+
+    @Test
+    public void testLeftBracketByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("[", new Token[] { new LeftBracketToken() });
+    }
+
+    @Test
+    public void testLeftCurlyByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("{", new Token[] { new LeftCurlyToken() });
+    }
+
+    @Test
+    public void testLeftParenthesisByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("(", new Token[] { new LeftParenthesisToken() });
+    }
+
+    @Test
+    public void testLengthByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("len", new Token[] { new LengthToken() });
+    }
+
+    @Test
+    public void testLessThanByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("<", new Token[] { new LessThanToken() });
+    }
+
+    @Test
+    public void testMinusByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("-", new Token[] { new MinusToken() });
+    }
+
+    @Test
+    public void testMultiplicationByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("*", new Token[] { new MultiplicationToken() });
+    }
+
+    @Test
+    public void testNewByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("new", new Token[] { new NewToken() });
+    }
+
+    @Test
+    public void testNotEqualByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("!=", new Token[] { new NotEqualToken() });
+    }
+
+    @Test
+    public void testPlusByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("+", new Token[] { new PlusToken() });
+    }
+
+    @Test
+    public void testPrintByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("println", new Token[] { new PrintToken() });
+    }
+
+    @Test
+    public void testReturnByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("return", new Token[] { new ReturnToken() });
+    }
+
+    @Test
+    public void testRightBracketByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("]", new Token[] { new RightBracketToken() });
+    }
+
+    @Test
+    public void testRightCurlyByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("}", new Token[] { new RightCurlyToken() });
+    }
+
+    @Test
+    public void testRightParenthesisByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes(")", new Token[] { new RightParenthesisToken() });
+    }
+
+    @Test
+    public void testSemiColonByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes(";", new Token[] { new SemiColonToken() });
+    }
+
     @Test
     public void testThisByItself() throws TokenizerException {
 
         // Check that tokenizing "this" works
         assertTokenizes("this", new Token[] { new ThisToken() });
     }
+
+    @Test
+    public void testVoidByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("Void", new Token[] { new VoidToken() });
+    }
+
+    @Test
+    public void testWhileByItself() throws TokenizerException {
+
+        // Check that tokenizing "this" works
+        assertTokenizes("while", new Token[] { new WhileToken() });
+    }
+
+
+
     // TODO: add the rests of the token tests such as above ^
+
+    @Test
+    public void testVariable() throws TokenizerException {
+        assertTokenizes("foo",
+                        new Token[] { new VariableToken("foo") });
+    }
 
     @Test
     public void testThisThisIsVariable() throws TokenizerException {
@@ -78,6 +290,7 @@ public class TokenizerTest {
                 new RightCurlyToken(),
                 new ElseToken(),
                 new IfToken(),
+                
         });
     }
 
