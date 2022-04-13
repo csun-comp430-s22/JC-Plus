517ab81954c12c79f9cbae4f53b962647c26d621
AlexisDiaz8706 <alexis.flanigan@gmail.com>
2022-04-12 23:56:01 -0700

    typechecker from oop example

---
+++ SourceCode/src/main/java/typechecker/TypeErrorException.java
@@ -0,0 +1,9 @@
+package typechecker;
+
+
+    public class TypeErrorException extends Exception {
+        public TypeErrorException(final String message) {
+            super(message);
+        }
+    }
+
